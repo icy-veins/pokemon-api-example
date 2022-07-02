8da4ba3bc1db02cf993756b7ce4d1f994a54043e
icy-veins <simple-developer@hotmail.com>
2022-07-02 17:28:40 +0100

    update - clean up x2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef } from 'react';
 import Input from './Input.js';
 import Card from './Card.js';
 function App() {
-  // const [ searchItem, setSearchItem ] = useState("pikachu");
   const [ pokemon, setPokemon ] = useState({});
   const renderCount = useRef(0);
 
@@ -28,18 +27,7 @@ function App() {
   useEffect(() => {
     getPokemon()
   }, [])
-  // useEffect(() => {
-  //   async function getPokemon() {
-  //     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchItem}`);
-  //     const data = await response.json();
-  //     const pokemonObj = {
-  //       title: data.name,
-  //       img: data.sprites.front_default
-  //     };
-  //     setPokemon(pokemonObj);
-  //   }
-  //   getPokemon();
-  // }, [searchItem])
+
   return (
     <div className="App">
         <Input updateSearch={updateSearch}/>
